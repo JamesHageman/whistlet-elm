7190057310e08d2866e9981a289b5e712ad45692
James Hageman <jamesd@hageman.ca>
2016-12-05 19:27:26 -0500

    initial tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,22 +3,15 @@ module Tests exposing (..)
 import Test exposing (..)
 import Expect
 import String
-import App
+import Data.RemoteData as RemoteData exposing (RemoteData(Success))
 
 
 all : Test
 all =
-    describe "A Test Suite"
-        [ test "App.model.message should be set properly" <|
-            \() ->
-                Expect.equal (App.init |> .route) "Your Elm App is working!"
-        , test "Addition" <|
-            \() ->
-                Expect.equal (3 + 7) 10
-        , test "String.left" <|
-            \() ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \() ->
-                Expect.fail "failed as expected!"
+    describe "Tests"
+        [ describe "RemoteData"
+            [ test "RemoteData.mapSuccess" <|
+                \() ->
+                    Expect.equal (RemoteData.mapSuccess ((+) 2) (Success 3)) (Success 5)
+            ]
         ]
