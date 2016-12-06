536c50e9e2d12871ef1d888174c1525811da3933
James Hageman <jamesd@hageman.ca>
2016-12-06 13:22:42 -0500

    add a comment

--- circle-deps.sh
+++ circle-deps.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Installs sysconfcpus so we can limit the cpus that elm uses to compile. 
+# Surprisingly, this makes elm compile much faster. 
+# See: https://github.com/elm-lang/elm-compiler/issues/1473#issuecomment-245704142 
+
 export INSTALL_PATH="$HOME/dependencies"
 
 if [ ! -d $INSTALL_PATH/sysconfcpus/bin ]; then
